e3ab4ab627f5da661bc50a9befaed9c9ddfdefc2
Batuhan Yaşar Yılmaz <batuhan@thebatuhansnetwork.xyz>
2023-10-25 09:51:26 +0300

    Added socket.io and message input

--- app/(main)/servers/[serverId]/channels/[channelId]/page.jsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.jsx
@@ -4,6 +4,7 @@ import { redirectToSignIn } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
 import ChatHeader from "@/components/chat/chat-header"
+import ChatInput from "@/components/chat/chat-input"
 
 async function ChannelIdPage({ params }) {
     const profile = await currentProfile()
@@ -34,6 +35,18 @@ async function ChannelIdPage({ params }) {
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">
+                Future Messages
+            </div>
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     )
